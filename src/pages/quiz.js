8c4e45ce753f2fa0be3cb42e863d87af4a25adea
이승원 <seungwon9509@gmail.com>
2023-10-19 18:33:47 +0900

    - quiz styling

--- src/pages/quiz.js
+++ src/pages/quiz.js
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 export default function Quiz(){
+  const quizText = "다음중 ㅇㅇㅇ으로 알맞은 것은? 질문의 길이가 너무 차이가 많이 나지 않게 해주시기 바랍니다";
 
   const questions = [
     {
@@ -40,15 +41,24 @@ export default function Quiz(){
 	}
 	
 	return (
-    <>
-      {questions.map((question, idx) => 
-        <li key={idx}>	
-          <label>
-            <input type="radio" name="name" value={question.value} onChange={onChange} />
-            {question.text}
-          </label>
-        </li>
-      )}
-    </>
+    <div className="main flex column between">
+      <div>
+        <h3>Q1.</h3>
+        <p>{quizText}</p>
+        <ol>
+          {questions.map((question, idx) => 
+            <li key={idx}>	
+              <label>
+                <input type="radio" name="name" value={question.value} onChange={onChange} />
+                {question.text}
+              </label>
+            </li>
+          )}
+        </ol>
+      </div>
+      <div className="flex">
+        <button onClick={onClick}>다음으로</button>
+      </div>
+    </div>
 	)
 }
